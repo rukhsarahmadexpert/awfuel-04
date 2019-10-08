29a4a3069da2d06e642cefaf7b7e2ddb83dd7587
Rukhsar Ahmad <itmolen1@gmail.com>
2019-10-08 12:14:09 +0400

    Rukhsar chnages

---
+++ IT.Core/ViewModels/CustomerOrderViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Core.ViewModels
+{
+    public class CustomerOrderViewModel
+    {
+        public int Id { get; set; }
+        public int OrderQuantity { get; set; }
+        public int VehicleId { get; set; }
+        public int DriverId { get; set; }
+        public int CompanyId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public string RequestThrough { get; set; }
+        public int MyProperty { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsActive { get; set; }
+        public bool Status { get; set; }
+        public string DriverName { get; set; }
+        public string VehicleNumber { get; set; }
+        public string UserName { get; set; }
+        public string CreateDates { get; set; }
+        public string Comments { get; set; }
+        public string CreatedTime { get; set; }
+        public string OrderProgress { get; set; }
+        public string CompanyName { get; set; }
+        public string AcceptDate { get; set; }
+        public string OrderDate { get; set; }
+        public string Name { get; set; }
+    }
+}
--- IT.Web/Controllers/CustomerOrderController.cs
+++ IT.Web/Controllers/CustomerOrderController.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using IT.Core.ViewModels;
+using IT.Repository.WebServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace IT.Web.Controllers
 {
     public class CustomerOrderController : Controller
     {
+
+        WebServices webServices = new WebServices();
+        List<CustomerOrderViewModel>  customerOrderViewModels = new List<CustomerOrderViewModel>();
+        CustomerOrderViewModel CustomerOrderViewModel = new CustomerOrderViewModel();
+
+
         public ActionResult Index()
         {
-            return View ();
+            PagingParameterModel pagingParameterModel = new PagingParameterModel();
+
+            pagingParameterModel.pageNumber = 1;
+            pagingParameterModel._pageSize = 1;
+            pagingParameterModel.CompanyId = 1055;
+            pagingParameterModel.pageSize = 100;
+
+            var CustomerOrderList = webServices.Post(pagingParameterModel, "CustomerOrder/All");
+
+            if (CustomerOrderList.StatusCode == System.Net.HttpStatusCode.Accepted)
+            {
+                customerOrderViewModels = (new JavaScriptSerializer().Deserialize<List<CustomerOrderViewModel>>(CustomerOrderList.Data.ToString()));
+            }
+
+            return View(customerOrderViewModels);
         }
     }
 }
