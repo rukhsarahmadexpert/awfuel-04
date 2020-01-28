8d47ac5f1b8e2deee1abde1b850a8866bd0bdc57
Rukhsar Ahmad <itmolen1@gmail.com>
2020-01-28 12:23:23 +0400

    Rukhsar changes

--- IT.Core/ViewModels/LPOInvoiceDetails.cs
+++ IT.Core/ViewModels/LPOInvoiceDetails.cs
@@ -12,7 +12,7 @@ namespace IT.Core.ViewModels
         public int UnitId { get; set; }
         public string Description { get; set; }
         public Decimal UnitPrice { get; set; }
-        public int IQunatityd { get; set; }
+        public int Qunatity { get; set; }
         public Decimal Total { get; set; }
         public Decimal VAT { get; set; }
         public Decimal SubTotal { get; set; }
